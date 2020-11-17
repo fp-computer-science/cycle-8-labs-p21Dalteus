2d51086c6211350b880aac290791500b27e70c16
Darick <p21dalteus@fairfieldprep.org>
2020-11-16 21:16:56 -0500

    newer update

---
+++ lab_4-1.py
@@ -0,0 +1,17 @@
+# Author: DA (AMDG) 11/16/2020
+
+colors = ["blue", "crimson", "orange", "green"]
+print(colors)
+
+colors.extend(["red", "ruby", "maroon"])
+print(colors)
+
+print(colors.count("yellow"))
+
+colors.insert(3, "mahogany")
+print(colors)
+
+colors.clear( )
+print(colors)
+
+print(colors.count("red"))
---
+++ lab_4-2.py
@@ -0,0 +1,17 @@
+# Author: DA (AMDG) 11/16/2020
+
+subjects = ['Geometry', 'Algebra II', 'Pre-Calculas']
+print(subjects)
+
+subjects.append('Calculas')
+print(subjects)
+
+print(subjects.index('Geometry'))
+
+subjects.sort( )
+print(subjects)
+
+subjects_2 = subjects.copy( )
+
+subjects_2.reverse( )
+print(subjects_2)
